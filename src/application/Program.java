8c1603b67021b5b38c15cb8fde6246cbbd361580
Vinicius Neves <vinicius.s.99821@gmail.com>
2022-07-02 21:04:41 -0300

    Expressão lambda inline

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,7 +4,7 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import java.util.function.Predicate;
+
 
 import entities.Product;
 
@@ -23,9 +23,7 @@ public class Program {
 
         double min = 100.0;
         
-        Predicate<Product> pred = p -> p.getPrice() >= min;
-        
-        list.removeIf(pred);
+        list.removeIf(p -> p.getPrice() >= min);
 
         for(Product p : list) {
 		System.out.println(p);
